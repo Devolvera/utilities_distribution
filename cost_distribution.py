34eaab80a13d9ba59cc8c5bc7ce186e1f294cb0c
Chrundle <donovan.olvera@gmail.com>
2022-04-17 16:27:01 -0700

    easter update

--- cost_distribution.py
+++ cost_distribution.py
@@ -1,10 +1,1 @@
-import csv
-
-gas = []
-
-with open('cost_distribution.csv') as data:
-    reader = csv.DictReader(data)
-    for row in reader:
-        gas.append(row['Gas'].strip('$'))
-        
-print(gas)
+# nothing for now
